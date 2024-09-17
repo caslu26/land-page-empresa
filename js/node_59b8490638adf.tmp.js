3e2bb82c1dffe4e5ad18e3945e2a39c7372e9793
Lucas da silva santiago <84160457+caslu26@users.noreply.github.com>
2024-09-17 00:24:38 -0300

    trocando layout

---
+++ js/node_59b8490638adf.tmp.js
@@ -0,0 +1,7 @@
+const input = require('readline-sync')
+
+const numero_sorteado = 5;
+
+const numero = Number(input.question('Qual numero voce escolheu '))
+
+console.log(numero, typeof numero);
---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* função para rolar ate a seção de orçamentos */
+function scrollToOrcamento() {
+    var orcamentoSection = document.getElementById('form');
+    orcamentoSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+/* função para rolar ate a seção de prjetos */
+function scrollToOrcamento() {
+    var orcamentoSection = document.getElementById('projects');
+    orcamentoSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+/* função para rolar ate a seção de Contatos */
+function scrollToOrcamento() {
+    var orcamentoSection = document.getElementById('contatos');
+    orcamentoSection.scrollIntoView({ behavior: 'smooth' });
+}
